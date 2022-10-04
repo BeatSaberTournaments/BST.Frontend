8a8dfb9b29ff5efcd3a67d203e75d131e1b213c4
NightHawk <hasfeldt@yahoo.dk>
2022-10-04 12:27:46 +0200

    Updated SQL to match new structure + tables

--- SQL.sql
+++ SQL.sql
@@ -12,9 +12,40 @@
  Target Server Version : 140005
  File Encoding         : 65001
 
- Date: 29/09/2022 21:40:21
+ Date: 04/10/2022 12:27:32
 */
 
+
+-- ----------------------------
+-- Sequence structure for apiauthkeys_id_seq
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."apiauthkeys_id_seq";
+CREATE SEQUENCE "public"."apiauthkeys_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Table structure for apiauthkeys
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."apiauthkeys";
+CREATE TABLE "public"."apiauthkeys" (
+  "id" int4 NOT NULL DEFAULT nextval('apiauthkeys_id_seq'::regclass),
+  "apikey" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "comment" varchar(255) COLLATE "pg_catalog"."default"
+)
+;
+COMMENT ON COLUMN "public"."apiauthkeys"."id" IS 'Auto-incrementing ID';
+COMMENT ON COLUMN "public"."apiauthkeys"."apikey" IS 'The actual API-key. First 4 characters should be BSTD';
+COMMENT ON COLUMN "public"."apiauthkeys"."comment" IS 'Who it was created for and by';
+
+-- ----------------------------
+-- Records of apiauthkeys
+-- ----------------------------
+INSERT INTO "public"."apiauthkeys" VALUES (2, 'BSTD-RAINE-E970-78290407285b', 'Raines DEV API-Key.');
+INSERT INTO "public"."apiauthkeys" VALUES (1, 'BSTD-HAWK0-E970-ca7247bc389d', 'Hawks DEV API-Key.');
 
 -- ----------------------------
 -- Table structure for banned_mods
@@ -94,7 +125,8 @@ CREATE TABLE "public"."tournaments" (
   "signupstatus" int2 DEFAULT 0,
   "twitchchannel" varchar(26) COLLATE "pg_catalog"."default" DEFAULT 'BeatSaberTournaments'::character varying,
   "public" bool DEFAULT false,
-  "image" varchar COLLATE "pg_catalog"."default"
+  "image" varchar COLLATE "pg_catalog"."default",
+  "apikey" varchar(255) COLLATE "pg_catalog"."default"
 )
 ;
 COMMENT ON COLUMN "public"."tournaments"."id" IS 'Numerical identifier for database use';
@@ -119,11 +151,12 @@ COMMENT ON COLUMN "public"."tournaments"."signupstatus" IS 'Numerical value for 
 COMMENT ON COLUMN "public"."tournaments"."twitchchannel" IS 'Name of the Twitch-channel';
 COMMENT ON COLUMN "public"."tournaments"."public" IS 'Boolean to decide if tournament show be shown or not';
 COMMENT ON COLUMN "public"."tournaments"."image" IS 'Image URL for image';
+COMMENT ON COLUMN "public"."tournaments"."apikey" IS 'A UUID key randomly generated to be the API key';
 
 -- ----------------------------
 -- Records of tournaments
 -- ----------------------------
-INSERT INTO "public"."tournaments" VALUES (1, 'BST Test Tournament', '[{"id":1,"scoresaberid":"76561198086326146","role":1},{"id":1,"scoresaberid":"76561198086326147","role":2}]', '[{"id":1,"scoresaberid":"76561198086326146","status":5},{"id":1,"scoresaberid":"76561198086326147","status":6}]', 0, '2023-01-01 00:06:40', '2023-02-28 00:06:47', '[{"id":1,"round":1,"image":"http:\/\/image.com","songs":[{"bsr":1,"diff":1},{"bsr":2,"diff":2}]}]', '[{"matchid":101,"player1":"76561198086326146","player2":"76561198086326147","score":[0,0],"round":1,"map-pool":1,"state":"pending","winner":"0"},{"matchid":102,"player1":"76561198086326146","player2":"76561198086326147","score":[1,0],"round":1,"map-pool":1,"state":"ongoing","winner":"0"},{"matchid":103,"player1":"76561198086326146","player2":"76561198086326147","score":[1,0],"round":1,"map-pool":1,"state":"ended","winner":"76561198086326146"}]', 0, 'BeatSaberTournaments', 't', 'https://danesaber.cc/assets/images/logo.png');
+INSERT INTO "public"."tournaments" VALUES (1, 'BST Test Tournament', '[{"id":1,"scoresaberid":"76561198086326146","role":1},{"id":1,"scoresaberid":"76561198086326147","role":2}]', '[{"id":1,"scoresaberid":"76561198086326146","status":5},{"id":1,"scoresaberid":"76561198086326147","status":6}]', 0, '2023-01-01 00:06:40', '2023-02-28 00:06:47', '[{"id":1,"round":1,"public":0,"qualifier":0,"image":"http://image.com","songs":[{"bsr":1,"diff":1},{"bsr":2,"diff":2}]}]', '[{"matchid":101,"player1":"76561198086326146","player2":"76561198086326147","score":[0,0],"round":1,"map-pool":1,"state":"pending","winner":"0"},{"matchid":102,"player1":"76561198086326146","player2":"76561198086326147","score":[1,0],"round":1,"map-pool":1,"state":"ongoing","winner":"0"},{"matchid":103,"player1":"76561198086326146","player2":"76561198086326147","score":[1,0],"round":1,"map-pool":1,"state":"ended","winner":"76561198086326146"}]', 0, 'BeatSaberTournaments', 't', 'https://danesaber.cc/assets/images/logo.png', 'BSTT-9877a-E970-977914e5c677');
 
 -- ----------------------------
 -- Table structure for users
@@ -152,8 +185,20 @@ COMMENT ON COLUMN "public"."users"."image" IS 'Users own image';
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO "public"."users" VALUES (1, 'Hawk', 10, '2022-09-28 21:34:25+02', 76561198086326146, 'thanighthawk', '592779895084679188', 'https://danesaber.cc/assets/images/logo.png');
 INSERT INTO "public"."users" VALUES (2, 'Hawk2', 10, '2022-09-28 21:34:25+02', 76561198086326147, 'thanighthawk2', '592779895084679188', 'https://danesaber.cc/assets/images/logo.png');
+INSERT INTO "public"."users" VALUES (1, 'Hawk', 10, '2022-09-28 21:34:25+02', 76561198086326146, 'thanighthawk', '592779895084679188', 'https://danesaber.cc/assets/images/logo.png');
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+ALTER SEQUENCE "public"."apiauthkeys_id_seq"
+OWNED BY "public"."apiauthkeys"."id";
+SELECT setval('"public"."apiauthkeys_id_seq"', 3, true);
+
+-- ----------------------------
+-- Primary Key structure for table apiauthkeys
+-- ----------------------------
+ALTER TABLE "public"."apiauthkeys" ADD CONSTRAINT "apiauthkeys_pkey" PRIMARY KEY ("id", "apikey");
 
 -- ----------------------------
 -- Primary Key structure for table banned_mods
